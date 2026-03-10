7db44e2ac09d8bff8d5d0f78e6bac145c9ec6f03
Avinash <avinash649556@gmail.com>
2026-03-10 09:15:56 +0530

    HiReplaceWithHello

---
+++ HiReplaceWithHello.java
@@ -0,0 +1,25 @@
+public class HiReplaceWithHello {
+    public static String hiReplaceWithHello(String str ){
+        if(str.length()<=1){
+            return str;
+        }
+        if(str.charAt(0)=='H' && str.charAt(1)=='i'){
+            String smallString = str.substring(2);
+            String result= hiReplaceWithHello(smallString);
+            return "hello"+ result;
+        }
+        else{
+            String smallString = str.substring(1);
+             String result=hiReplaceWithHello(smallString); 
+             return str.charAt(0)+result;
+        }
+
+    }
+    public static void main(String[] args) {
+        String str = "Hi Avinash Hi Avinash";
+       // String replace="Hello";
+        System.out.println(hiReplaceWithHello( str));
+
+    }
+    
+}
