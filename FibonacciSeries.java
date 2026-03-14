c8ee68a0304dd2de85074fec4fbdb982a23eb024
Avinash <avinash649556@gmail.com>
2026-03-14 22:21:29 +0530

    Fibonacci Number

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -1,8 +1,11 @@
 public class FibonacciSeries {
     public static int fibo(int num){
-        
+        if(num<=1){
+            return num;
+        }
+        return fibo(num-1)+fibo(num-2);
     }
     public static void main(String[] args) {
-        
+         System.out.println(fibo(8));
     }
 }
