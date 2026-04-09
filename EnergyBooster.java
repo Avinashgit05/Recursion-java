c79dd13a880fab03fc3d0b6e60462e2e383e5b51
Avinash <avinash649556@gmail.com>
2026-04-09 21:00:27 +0530

    EnergyBooster

---
+++ EnergyBooster.java
@@ -0,0 +1,15 @@
+public class EnergyBooster {
+    static long power(long a, int n) {
+        if (n == 0) return 1;
+        return a * power(a, n - 1);
+    }
+    public static void main(String[] args) {
+
+        long a = 2;   
+        int n = 5;    
+
+        long result = power(a, n);
+
+        System.out.println("Power is: " + result);
+    }
+}
