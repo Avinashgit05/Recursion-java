54a763b80b3735ecacf01eb1558382f64e4a8128
Avinash <avinash649556@gmail.com>
2026-04-10 22:39:53 +0530

    AnnouncName_N_Times

---
+++ AnnouncName_N_Times.java
@@ -0,0 +1,12 @@
+public class AnnouncName_N_Times {
+    public static void printName(int n, String name) {
+    if (n == 0) return;
+    
+    System.out.println(name);
+    printName(n - 1, name); 
+}
+public static void main(String[] args) {
+    printName(5, "Avinash");
+}
+    
+}
--- EnergyBooster.java
+++ EnergyBooster.java
@@ -4,12 +4,10 @@ public class EnergyBooster {
         return a * power(a, n - 1);
     }
     public static void main(String[] args) {
-
         long a = 2;   
         int n = 5;    
-
         long result = power(a, n);
-
         System.out.println("Power is: " + result);
     }
 }
+// energy booster.
